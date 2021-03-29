12d4b56849749b4dad226c44cc63da3da9059d14
Lauren Chambers <laurenchambers11@icloud.com>
2021-03-29 18:46:22 -0500

    created store and added dependencies

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+// import * as sessionActions from "./store/session";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  // window.sessionActions = sessionActions;
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
