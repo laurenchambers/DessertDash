9f3617138932018fff92aa6e48e8a950090eaa52
Lauren Chambers <laurenchambers11@icloud.com>
2021-03-30 15:27:48 -0500

    created models

---
+++ app/models/restaurantgenre.py
@@ -0,0 +1,10 @@
+from .db import db
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+restaurants_genres = db.Table(
+    "restaurants_genres",
+    db.Column("restaurant_id", db.Integer, ForeignKey(restaurants.id), primary_key=True),
+    db.Column("genre_id", db.Integer, ForeignKey("genres.id"), primary_key=True))
