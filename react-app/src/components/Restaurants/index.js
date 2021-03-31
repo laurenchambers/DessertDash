f69e67de01ab99ed19887b6821d59c5383d8c6e5
Lauren Chambers <laurenchambers11@icloud.com>
2021-03-31 18:18:11 -0500

    did genre route and starting on landing page

--- react-app/src/components/Restaurants/index.js
+++ react-app/src/components/Restaurants/index.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { allRestaurants } from "../../store/restaurants";
 import { getAllItems } from "../../store/items";
 import Restaurant from "../Restaurant";
-import Item from "../Item";
 
 const ShowRestaurant = () => {
   const dispatch = useDispatch();
@@ -12,9 +11,7 @@ const ShowRestaurant = () => {
   );
 
   const itemsArray = useSelector((state) => state?.items?.allItems?.items);
-  console.log("ITEMs", itemsArray);
 
-  console.log("ARRAY", restaurantArray);
   useEffect(() => {
     dispatch(allRestaurants());
     dispatch(getAllItems());
@@ -27,11 +24,6 @@ const ShowRestaurant = () => {
           <Restaurant key={restaurant.id} restaurant={restaurant} />
         ))}
       </h1>
-      <h2>
-        {itemsArray?.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </h2>
     </>
   );
 };
