9b595b2449eb2333e13ee2c55c57bf8ff3d664da
Lauren Chambers <laurenchambers11@icloud.com>
2021-04-01 13:09:34 -0500

    navbar inital styling and created the meny drop down

---
+++ react-app/src/components/NavBar/MenuBar.js
@@ -0,0 +1,67 @@
+import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import menubar from "../site-images/menu-bar.png";
+import greyx from "../site-images/grey-x.png";
+import homeicon from "../site-images/home-icon.png";
+import profileicon from "../site-images/profile-icon.jpeg";
+import carticon from "../site-images/cart-icon.jpeg";
+import "./MenuBar.css";
+
+function MeunBar({ setAuthenticated }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  const openMenu = () => {
+    if (showMenu) {
+      setShowMenu(false);
+    } else {
+      setShowMenu(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    document.addEventListener("mouseEnter", closeMenu);
+
+    return () => document.removeEventListener("mouseEnter", closeMenu);
+  }, [showMenu]);
+
+  return (
+    <div className="menubar-container">
+      <img className="profile-img" src={menubar} alt="" onClick={openMenu} />
+      {showMenu && (
+        <div className="menu">
+          <div onClick={closeMenu}>
+            <img src={greyx} alt="" className="menubar-x" />
+          </div>
+          <div onClick={closeMenu}>
+            <NavLink to="/home" exact={true} className="dropdown-item-first">
+              <img className="menubar-image" src={homeicon} alt="" />
+              Home
+            </NavLink>
+          </div>
+          <div onClick={closeMenu} >
+            <NavLink to="/checkout" exact={true} className="dropdown-item">
+              <img src={carticon} alt="" />
+              Checkout
+            </NavLink>
+          </div>
+          <div onClick={closeMenu}>
+            <NavLink to="/logout" exact={true} className="dropdown-item">
+              <img className="menubar-image" src={profileicon} alt="" />
+              Sign Out
+            </NavLink>
+          </div>
+          {/* <div className="logout-button-nav">
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </div> */}
+        </div>
+      )}
+    </div>
+  );
+}
+export default MeunBar;
---
+++ react-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "../auth/LogoutButton";
+import "./NavBar.css";
+import dessertdash_logo from "../site-images/desssertdash-logo.jpg";
+import MeunBar from "./MenuBar";
+
+const NavBar = ({ setAuthenticated }) => {
+  return (
+    <div>
+      <div className="navbar-container">
+        <nav className="navbar-navgation">
+          <div>
+            <div className="navbar-toggle-menu">
+              <MeunBar />
+            </div>
+          </div>
+          <div className="nav-bar-address">INSERT COMPONENT FOR ADDRESS</div>
+          <div className="navbar-logo">
+            <NavLink
+              className="navbar-logo"
+              to="/home"
+              exact={true}
+              activeClassName="active"
+            >
+              <img className="navbar-image" src={dessertdash_logo} alt="" />
+              DESSERTDASH
+            </NavLink>
+          </div>
+          <div className="narbar-search-container">SEARCH BAR</div>
+          <div className="navbar-cart-container">CART</div>
+          {/* <ul>
+            <li>
+            </li>
+            <li>
+              <NavLink to="/login" exact={true} activeClassName="active">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                Sign Up
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" exact={true} activeClassName="active">
+                Users
+              </NavLink>
+            </li>
+            <li>
+              <LogoutButton setAuthenticated={setAuthenticated} />
+            </li>
+          </ul> */}
+        </nav>
+      </div>
+      {/* div below goes with navbar container */}
+    </div>
+  );
+};
+
+export default NavBar;
