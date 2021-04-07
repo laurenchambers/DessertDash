4e6c8db2fd5c99ed653a7faffa7fea6494e3fc72
Lauren Chambers <laurenchambers11@icloud.com>
2021-04-06 20:50:57 -0500

    modal and post route for adding to cart

---
+++ react-app/src/components/ItemModal/ItemForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { updateItem } from "../../store/items";
+import greyx from "../site-images/grey-x.png";
+import "./ItemForm.css";
+
+const ItemForm = ({ item, setShowModal }) => {
+  const user = useSelector((state) => state.session.user);
+  //   const restaurant = useSelector((state) => state.restaurant.currentRestaurant);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [quantity, setQuantity] = useState(1);
+
+  //   useEffect(() => {
+  //     setQuantity;
+  //   }, [quantity]);
+
+  const handleSubmit = () => {
+    const submission = {
+      user_id: user.id,
+      item_id: item.id,
+      quantity,
+    };
+    console.log("submission", submission);
+    dispatch(updateItem(submission));
+  };
+
+  return (
+    <div className="item-form-container">
+      <div className="item-form-title">
+        <p>{item.name}</p>
+        <div
+          onClick={() => setShowModal(false)}
+          className="sip-form-title-close"
+        >
+          <img src={greyx} alt="" />
+        </div>
+      </div>
+      <div className="item-form-description-container">
+        <div className="item-form-description-box">{item.description}</div>
+        <div className="item-form-picture-container">
+          <div className="item-form-picture">
+            <img src={item.image_src} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="item-form-button-container">
+        <div className="item-form-button">
+          <button onClick={handleSubmit}>Add to cart ${item.price}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemForm;
