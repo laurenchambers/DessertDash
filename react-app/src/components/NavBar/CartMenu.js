1ac63df183b8ab131504a474d749ca635a94e042
Lauren Chambers <laurenchambers11@icloud.com>
2021-04-16 18:11:01 -0500

    checkut bar fixes

--- react-app/src/components/NavBar/CartMenu.js
+++ react-app/src/components/NavBar/CartMenu.js
@@ -35,6 +35,9 @@ function Cart({ cart, submission, authenticated }) {
   console.log("cartsarray", cartsArray);
   useEffect(() => {
     dispatch(getAllCarts());
+    if (!cartsArray?.length) {
+      return [];
+    }
     if (!showCart) return;
 
     document.addEventListener("mouseEnter", closeCart);
@@ -42,76 +45,97 @@ function Cart({ cart, submission, authenticated }) {
     return () => document.removeEventListener("mouseEnter", closeCart);
   }, [dispatch, showCart]);
 
-  return (
-    <>
-      <img className="cart-image" src={carticon} alt="" onClick={openCart} />
-      {/* <div className="cart-container"> */}
-      {/* <div className="cart-image-container">
-      </div> */}
-      {showCart && (
-        <div className="cart">
-          <div onClick={closeCart}>
-            <img src={greyx} alt="" className="menubar-x" />
-          </div>
-          <div className="cart-your-order">Your Order</div>
-          <div onClick={closeCart}>
-            <NavLink to="/checkout" exact={true} className="cart-checkout">
-              <button className="cart-checkout-button">Checkout</button>
-            </NavLink>
-          </div>
-          <div className="cart-current-items">
-            {cartsArray?.map((cart) => (
-              <>
-                <div className="cart-items-container">
-                  <div className="cart-current-item-quantity">
-                    <span classname="current-item-quantity-cart">
-                      {cart.quantity} x
-                    </span>
-                  </div>
-                  <div className="cart-current-item-name">
-                    <span className="curent-item-name-cart">
-                      {cart.item_name}
-                    </span>
-                    <div className="cart-current-item-preferences">
-                      {cart.preferences}
+  if (cartsArray?.length) {
+    return (
+      <>
+        <img className="cart-image" src={carticon} alt="" onClick={openCart} />
+        {showCart && (
+          <div className="cart">
+            <div onClick={closeCart}>
+              <img src={greyx} alt="" className="menubar-x" />
+            </div>
+            <div className="cart-your-order">Your Order</div>
+            <div onClick={closeCart}>
+              <NavLink to="/checkout" exact={true} className="cart-checkout">
+                <button className="cart-checkout-button">Checkout</button>
+              </NavLink>
+            </div>
+            <div className="cart-current-items">
+              {cartsArray?.map((cart) => (
+                <>
+                  <div className="cart-items-container">
+                    <div className="cart-current-item-quantity">
+                      <span classname="current-item-quantity-cart">
+                        {cart.quantity} x
+                      </span>
+                    </div>
+                    <div className="cart-current-item-name">
+                      <span className="curent-item-name-cart">
+                        {cart.item_name}
+                      </span>
+                      <div className="cart-current-item-preferences">
+                        {cart.preferences}
+                      </div>
+                    </div>
+                    <div className="cart-current-item-remove">
+                      <button
+                        onClick={() => {
+                          dispatch(removeFromCart(cart.id));
+                        }}
+                        className="remove-button"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => setShowModal(cart.id)}
+                        className="edit-item-button"
+                      >
+                        Edit
+                      </button>
+                      {cart.id === showModal && (
+                        <Modal onClose={() => setShowModal(null)}>
+                          <EditItemForm
+                            cart={cart}
+                            setShowModal={setShowModal}
+                          />
+                        </Modal>
+                      )}
+                    </div>
+                    <div className="cart-current-item-price">
+                      <span className="current-item-price">
+                        ${(cart.item_price * cart.quantity).toFixed(2)}
+                      </span>
                     </div>
                   </div>
-                  <div className="cart-current-item-remove">
-                    <button
-                      onClick={() => {
-                        dispatch(removeFromCart(cart.id));
-                      }}
-                      className="remove-button"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                  <div>
-                    <button
-                      onClick={() => setShowModal(cart.id)}
-                      className="edit-item-button"
-                    >
-                      Edit
-                    </button>
-                    {cart.id === showModal && (
-                      <Modal onClose={() => setShowModal(null)}>
-                        <EditItemForm cart={cart} setShowModal={setShowModal} />
-                      </Modal>
-                    )}
-                  </div>
-                  <div className="cart-current-item-price">
-                    <span className="current-item-price">
-                      ${(cart.item_price * cart.quantity).toFixed(2)}
-                    </span>
-                  </div>
-                </div>
-              </>
-            ))}
+                </>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-      {/* </div> */}
-    </>
-  );
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <img className="cart-image" src={carticon} alt="" onClick={openCart} />
+        {showCart && (
+          <div className="cart">
+            <div onClick={closeCart}>
+              <img src={greyx} alt="" className="menubar-x" />
+            </div>
+            <div onClick={closeCart}></div>
+            <div className="empty-cart-container">
+              <div className="empty-cart">Your cart is empty</div>
+              <div className="add-items-empty-cart">
+                Add items to get started
+              </div>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 export default Cart;
--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
 
-const LogoutButton = ({ setAuthenticated }) => {
+const LogoutButton = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
 
   const onLogout = async (e) => {
     await logout();
+    setAuthenticated(false);
     dispatch(logout());
-    // setAuthenticated(false);
     return history.push("/");
   };
 
