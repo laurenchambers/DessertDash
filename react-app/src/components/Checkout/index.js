a24187869948f768e4b227edfeb0b2af73b5f0b0
Lauren Chambers <laurenchambers11@icloud.com>
2021-04-23 10:24:21 -0500

    finally fixed tthe marker issue with googlemap

--- react-app/src/components/Checkout/index.js
+++ react-app/src/components/Checkout/index.js
@@ -8,13 +8,31 @@ import mastercard from "../site-images/mastercard.png";
 
 const Map = withGoogleMap(() => {
   const user = useSelector((state) => state.session.user);
-  const defaultMarker = { lat: user.lat, lng: user.lng };
+  const locations = [
+    {
+      name: "Location 1",
+      location: {
+        lat: user.lat,
+        lng: user.lng,
+      },
+    },
+    {
+      name: "Location 2",
+      location: {
+        lat: 41.3917,
+        lng: 2.1649,
+      },
+    },
+  ];
+
   return (
     <GoogleMap
       defaultZoom={14}
       defaultCenter={{ lat: user.lat, lng: user.lng }}
     >
-      {<Marker key={user.id} postition={defaultMarker} />}
+      {locations.map((item) => {
+        return <Marker key={item.name} position={item.location} />;
+      })}
     </GoogleMap>
   );
 });
